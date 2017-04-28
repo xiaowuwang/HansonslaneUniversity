8834be66cf5dab924e196467e1d9275d1cd50ba6
steven wang <stevenwang@hotmail.co.nz>
2017-04-28 18:33:26 +1200

    sorting, filtering, paging, and grouping

--- HansonslaneUniversity/Controllers/HomeController.cs
+++ HansonslaneUniversity/Controllers/HomeController.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using HansonslaneUniversity.Data;
+using HansonslaneUniversity.Models.SchoolViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace HansonslaneUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolDbContext _context;
+
+        public HomeController(SchoolDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
