a05b01c12111637e48c9ead650465a2e810e22d1
steven wang <stevenwang@hotmail.co.nz>
2017-04-29 10:48:23 +1200

    Handling concurrency conflicts

--- HansonslaneUniversity/Data/Migrations/20170428222655_RowVersion.Designer.cs
+++ HansonslaneUniversity/Data/Migrations/20170428222655_RowVersion.Designer.cs
@@ -8,9 +8,10 @@ using HansonslaneUniversity.Data;
 namespace HansonslaneUniversity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170428222655_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
